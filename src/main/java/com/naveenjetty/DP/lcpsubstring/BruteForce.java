9aaecaef74ceabeebdcc163887bcea8f2d135719
Naveen Jetty <naveenjetty31@gmail.com>
2017-01-09 10:01:19 -0500

    LCP

--- src/main/java/com/naveenjetty/DP/lcpsubstring/BruteForce.java
+++ src/main/java/com/naveenjetty/DP/lcpsubstring/BruteForce.java
@@ -1,7 +1,58 @@
-package com.naveenjetty.DP.fibonacci.lcpsubstring;
+package com.naveenjetty.DP.lcpsubstring;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
  * Created by naveenjetty on 1/3/17.
  */
 public class BruteForce {
+    public String longestPalindrome(String s) {
+        Set<String> palins = new LinkedHashSet<String>();
+        getList(s,palins);
+        String res=null;
+        for (String str: palins) {
+            if (res==null){
+                res=str;
+            } else{
+                if (res.length()<str.length()) res=str;
+            }
+        }
+        return res;
+    }
+
+    public void getList(String s, Set<String> palins){
+        if (s==null || s.length()==0) return;
+        if (s.length()==1) {
+            palins.add(s);
+            return;
+        }
+        if (isPalindrome(s)) {
+            palins.add(s);
+            return;
+        }
+        String s1 = s.substring(0,s.length()-1);
+        String s2 = s.substring(1);
+        getList(s1,palins);
+        getList(s2,palins);
+
+    }
+
+    public boolean isPalindrome(String s){
+        int n = s.length();
+        for (int i = 0; i < (n/2); ++i) {
+            if (s.charAt(i) != s.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        BruteForce bf = new BruteForce();
+        String s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+        String ps = bf.longestPalindrome(s);
+        System.out.println(ps);
+    }
+
 }
--- src/main/java/com/naveenjetty/DP/lcpsubstring/Simple.java
+++ src/main/java/com/naveenjetty/DP/lcpsubstring/Simple.java
@@ -4,4 +4,34 @@ package com.naveenjetty.DP.lcpsubstring;
  * Created by naveenjetty on 1/3/17.
  */
 public class Simple {
+    int maxLen,  li;
+
+    public String longestPalindrome(String s) {
+        if (s==null || s.length()==0) return null;
+        int len = s.length();
+        if (len==1) return s;
+        for(int i=0;i<len;i++){
+            extendPalindrome(s,i,i);
+            extendPalindrome(s,i,i+1);
+        }
+        return s.substring(li,li+maxLen);
+    }
+
+    void extendPalindrome(String s,int j, int k){
+        while(j>=0 && k<s.length() && s.charAt(j)==s.charAt(k)){
+            j--;
+            k++;
+        }
+        if (maxLen < k-j-1){
+            li = j+1;
+            maxLen = k-j-1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Simple bf = new Simple();
+        String s = "civilwartestingwhetherthatnaptionoranyn";
+        String ps = bf.longestPalindrome(s);
+        System.out.println(ps);
+    }
 }
